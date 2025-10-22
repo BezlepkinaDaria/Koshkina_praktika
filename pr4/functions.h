8b4813853db6746dd14610a929bba9634a0f84d6
BezlepkinaDaria <daschab2015@gmail.com>
2025-10-22 23:34:57 +0300

    pr4

---
+++ pr4/functions.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+int bitwiseAND(int a, int b);
+int bitwiseOR(int a, int b);
+int bitwiseXOR(int a, int b);
+long long factorial(int n);
+double power(double base, int exp);
+void calculatorMenu();
+
+void nutritionTracker();
+
+void salaryAnalysis();
+
+void temperatureAnalysis();
+
+void financeAnalysis();
+
+void mainMenu();
